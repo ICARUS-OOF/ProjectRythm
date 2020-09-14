3ddd9312ffea1285f8993ead658e32f2a8693405
ICARUS-OOF <kaydenchai2008@gmail.com>
2020-09-14 21:43:16 +0800

    Cascade parts and player eyes

--- Colour Core/Assets/Scripts/Data/PlayerData.cs
+++ Colour Core/Assets/Scripts/Data/PlayerData.cs
@@ -9,13 +9,8 @@ namespace ColorCore
         public class PlayerData : MonoBehaviour
         {
             public int Health = 3;
-            public GameObject HealthItem1, HealthItem2, HealthItem3;
             public void Damage()
             {
-                Health--;
-                HealthItem1.SetActive(Health >= 3);
-                HealthItem2.SetActive(Health >= 2);
-                HealthItem3.SetActive(Health >= 1);
                 if (Health <= 0)
                 {
 
